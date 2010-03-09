bfb715b0c4398dcdb170f55ed1f63544eed86721
Nigel Atkinson <nigel@atkinson.gen.nz>
2010-03-10 07:53:37 +1300

    Added routine to get mesh data.
    Added in new src file ogretools.cpp
    Will be used in mouse picking and conversion of navigation mesh from
    Ogre to our own representation.
    	new file:   include/ogretools.h
    	new file:   src/ogretools.cpp

---
+++ include/ogretools.h
@@ -0,0 +1,19 @@
+#ifndef __OGRETOOLS_H
+#define __OGRETOOLS_H
+
+#include <OgreMesh.h>
+#include <OgreSubMesh.h>
+
+namespace OgreTools
+{
+    void GetMeshInformation(const Ogre::MeshPtr mesh,
+            size_t &vertex_count,
+            Ogre::Vector3* &vertices,
+            size_t &index_count,
+            unsigned long* &indices,
+            const Ogre::Vector3 &position,
+            const Ogre::Quaternion &orient,
+            const Ogre::Vector3 &scale);
+}
+
+#endif // __OGRETOOLS_H
