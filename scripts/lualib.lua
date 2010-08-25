33a601a4068bf3ac4214cc18816b7e72a276622d
Nigel Atkinson <nigel@atkinson.gen.nz>
2010-08-26 00:11:01 +1000

    Added name finder for enhanced 'showTasks'.
    Also will aid in debugging.

--- scripts/lualib.lua
+++ scripts/lualib.lua
@@ -30,3 +30,39 @@ function round( num, digits )
     local shift = 10 ^ digits
     return math.floor( num * shift + 0.5 ) / shift
 end
+
+-- Given a reference (usally a function) find the first
+-- "name" in the Global name space.
+function getName( ref, skip )
+	local visited = {}
+	if type(skip)=='table' then -- copy skip table
+		for k,v in pairs(skip) do
+			visited[k]=v
+		end
+	end
+	return searchTableHier( ref, visited, _G )
+end
+
+function searchTableHier( ref, visited, t )
+
+	visited[t] = true
+
+	-- Search table for function
+	for k,v in pairs(t) do
+		if v == ref then
+			return k
+		end
+	end
+	
+	-- Not found, try each table (if any) within t recursively
+	for k,v in pairs(t) do
+		if type(v) == 'table' and visited[v] ~= true then
+			local ret = searchTableHier( ref, visited, v )
+			if ret then
+				return k .. '.' .. ret
+			end
+		end
+	end
+			
+	return nil
+end
