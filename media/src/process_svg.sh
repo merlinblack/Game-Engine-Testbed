223a711180aa554769641de5c07402e5b4fe335b
Nigel Atkinson <merlinblack@users.noreply.github.com>
2010-09-28 20:08:42 +1000

    Automated generation of Gorilla atlas.
    Not including fonts.  Fits sprites using a simple BSP huristic.

---
+++ media/src/process_svg.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+convert()
+{
+    svg=$1
+    width=$2
+    height=$3
+    png=$(basename $1 .svg).png
+    echo Converting $svg to $png Width:$width Height:$height
+    inkscape $svg --export-png=$png -w$width -h$height
+}
+
+convert icon.chk.green.svg 16 16 
+convert icon.chk.red.svg 16 16 
+convert icon.exit.svg 16 16 
+convert icon.info.svg 16 16 
+convert button.hover.svg 128 32
+convert button.normal.svg 128 32
+convert button.pressed.svg 128 32
