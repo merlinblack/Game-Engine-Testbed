b7ccd6df4354ca0cd40aea1548f0660463e600c9
Nigel Atkinson <merlinblack@users.noreply.github.com>
2011-05-23 00:09:17 +1000

    Version string
    Pull various version strings together, and made availible to Lua

---
+++ src/version.cpp
@@ -0,0 +1,61 @@
+/*
+-----------------------------------------------------------------------------
+Copyright (c) 2010 Nigel Atkinson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+
+#include <lua.h>
+#include <luabind/version.hpp>
+#include <OgrePrerequisites.h>
+#include <OgreString.h>
+#include <OgreStringConverter.h>
+
+Ogre::String versionString()
+{
+    Ogre::String version;
+
+    version = "Game Engine Testbed 0.2\n";
+
+    version += "Ogre ";
+    version += Ogre::StringConverter::toString( OGRE_VERSION_MAJOR );
+    version += ".";
+    version += Ogre::StringConverter::toString( OGRE_VERSION_MINOR );
+    version += ".";
+    version += Ogre::StringConverter::toString( OGRE_VERSION_PATCH );
+    version += " ";
+    version += OGRE_VERSION_NAME;
+    version += " ";
+    version += OGRE_VERSION_SUFFIX;
+    version += "\n";
+
+    version += LUA_RELEASE;
+    version += "\n";
+
+    version += "Luabind ";
+    version += Ogre::StringConverter::toString(LUABIND_VERSION / 10000);
+    version += ".";
+    version += Ogre::StringConverter::toString(LUABIND_VERSION / 100 % 100); 
+    version += ".";
+    version += Ogre::StringConverter::toString(LUABIND_VERSION % 100); 
+    version += "\n";
+
+    return version;
+}
