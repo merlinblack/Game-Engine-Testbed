4cb9bc07498449615730c26153562623bf53387b
Nigel Atkinson <merlinblack@users.noreply.github.com>
2013-06-25 16:56:53 +1000

    Support for making read only Lua tables
    Used to make tables of Enums.

---
+++ include/luareadonlytable.h
@@ -0,0 +1,9 @@
+#include <lua.hpp>
+#include <LuaBridge.h>
+
+// Creates a proxy table that allows only reading the values to the input table.
+// The input table is not altered, and any reference to it sill has read and write
+// access.
+// The proxy table supports iteration via pairs()
+//
+luabridge::LuaRef makeReadonlyProxy( lua_State* L, luabridge::LuaRef table );
--- src/gorillabind.cpp
+++ src/gorillabind.cpp
@@ -1,5 +1,6 @@
 #include <lua.hpp>
 #include <LuaBridge.h>
+#include <luareadonlytable.h>
 #include <Gorilla.h>
 #include <ward_ptr.h>
 
@@ -54,59 +55,6 @@ void destroyMarkupText( Layer *layer, ward_ptr<MarkupText> ptr )
     ptr.invalidate();
 }
 
-int readonlyTableError( lua_State* L )
-{
-    return luaL_error( L, "Attempt to modify a readonly table." );
-}
-
-int readonlyTableNext( lua_State* L )
-{
-    lua_getmetatable( L, 1 ); // Get read only table's metatable
-    lua_getfield( L, 3, "__index" ); // Get actual table
-    lua_pushvalue( L, 2 );
-    if( lua_next( L, 4 ) )
-    {
-        lua_remove( L, 3 ); // Meta table
-        lua_remove( L, 3 ); // Real table
-        lua_remove( L, 2 ); // Input index
-        lua_remove( L, 1 ); // Input table
-        return 2;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-int getReadonlyTableNext( lua_State* L )
-{
-    lua_pushcfunction( L, readonlyTableNext );
-    lua_insert( L, 1 );
-    lua_pushnil( L );
-
-    return 3;
-}
-
-LuaRef makeTableReadonly( lua_State* L, LuaRef table )
-{
-    if( table.isTable() == false )
-        throw std::invalid_argument( "makeTableReadOnly not given table" );
-
-    LuaRef metatable = newTable( L );
-    metatable["__index"] = table;
-    metatable["__newindex"] = readonlyTableError;
-    metatable["__metatable"] = false;
-    metatable["__pairs"] = getReadonlyTableNext;
-
-    LuaRef proxy = newTable( L );
-    proxy.push( L );
-    metatable.push( L );
-    lua_setmetatable( L, -2 );
-    lua_pop( L, 1 );
-
-    return proxy;
-}
-
 void bindGorilla( lua_State* L )
 {
     LuaRef gorillaEnum = newTable( L );
@@ -115,33 +63,33 @@ void bindGorilla( lua_State* L )
     gradient["NorthSouth"] = (int)Gradient_NorthSouth;
     gradient["WestEast"]   = (int)Gradient_WestEast;
     gradient["Diagonal"]   = (int)Gradient_Diagonal;
-    gorillaEnum["Gradient"] = makeTableReadonly( L, gradient );
+    gorillaEnum["Gradient"] = makeReadonlyProxy( L, gradient );
 
     LuaRef border = newTable( L );
     border["North"] = (int)Border_North;
     border["East"]  = (int)Border_East;
     border["South"] = (int)Border_South;
     border["West"]  = (int)Border_West;
-    gorillaEnum["Border"] = border;
+    gorillaEnum["Border"] = makeReadonlyProxy( L, border);
 
     LuaRef corner = newTable( L );
     corner["TopLeft"]     = (int)TopLeft;
     corner["TopRight"]    = (int)TopRight;
     corner["BottomRight"] = (int)BottomRight;
     corner["BottomLeft"]  = (int)BottomLeft;
-    gorillaEnum["Corner"] = corner;
+    gorillaEnum["Corner"] = makeReadonlyProxy( L, corner );
 
     LuaRef textalign = newTable( L );
     textalign["Left"]   = (int)TextAlign_Left;
     textalign["Centre"] = (int)TextAlign_Centre;
-    textalign["Right"]  = (int)TextAlign_Left;
-    gorillaEnum["TextAlign"] = textalign;
+    textalign["Right"]  = (int)TextAlign_Right;
+    gorillaEnum["TextAlign"] = makeReadonlyProxy( L, textalign );
 
     LuaRef vertalign = newTable( L );
     vertalign["Top"]    = (int)VerticalAlign_Top;
     vertalign["Middle"] = (int)VerticalAlign_Middle;
     vertalign["Bottom"] = (int)VerticalAlign_Bottom;
-    gorillaEnum["VerticalAlign"] = vertalign;
+    gorillaEnum["VerticalAlign"] = makeReadonlyProxy( L, vertalign );
 
     getGlobalNamespace( L )
         .beginNamespace( "Gorilla" )
@@ -151,6 +99,6 @@ void bindGorilla( lua_State* L )
 
     LuaRef gorillaNS = getGlobal( L, "Gorilla" );
 
-    gorillaNS["Enum"] = gorillaEnum;
+    gorillaNS["Enum"] = makeReadonlyProxy( L, gorillaEnum );
 
 }
