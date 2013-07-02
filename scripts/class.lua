9e0aad398aa432231ca4c8208e026f1a9213335f
Nigel Atkinson <merlinblack@users.noreply.github.com>
2013-07-02 15:19:59 +1000

    New Lua class implementation
    Designed to mimick the 'class' function from Luabind as far as use.

---
+++ scripts/class.lua
@@ -0,0 +1,48 @@
+-- Lua Class
+--
+-- Creates a class definition to have members added to
+--
+-- Example use:
+--
+-- class 'base'
+--
+-- function base:__init()       -- will be called on construction
+--   self.xyz = 42
+-- end
+--
+-- class 'derived' (base)       -- will inheirit base's members unless overridden
+--
+-- function derived:something( x )
+--   self.xyz = x
+--   self.abc = 'Hello World'
+-- end
+--
+
+
+function class( name )
+    _G[name] = {
+        __type = name,
+        __luaclass = true
+    }
+
+    local newclass = _G[name]
+
+    local metatable =
+    {
+        __call = function(...)
+            local obj = {}
+
+            setmetatable( obj, { __index = newclass } )
+
+            if obj.__init ~= nil then
+                obj:__init( select(2, ...) )
+            end
+
+            return obj
+        end
+    }
+
+    setmetatable( newclass, metatable )
+
+    return function( base ) metatable['__index'] = base end
+end
